afa551e3a2622a85a802421b3b007e447f4ef3c2
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-19 17:20:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,12 @@
 class Song
-  f
+  attr_accessor :total
+  attr_accessor :discount
+  attr_accessor :items
+  
+  def initialize(discount=nil)
+    @total = 0
+    @discount=discount
+    @items=[]
+  end
   
 end
